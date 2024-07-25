a12cbb844381ca0b6c09ea65d50f3cb4e97ba70c
supersuryaansh <prod.genx@gmail.com>
2024-07-25 10:07:03 +0530

    Minor changes to code structure

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ import Hyperswarm from "hyperswarm";
 import Hypercore from "hypercore";
 import b4a from "b4a";
 
-const swarm = new Hyperswarm();
-// Initialise hypercore in hyperclip-dir
-const core = new Hypercore("./hyperclip-dir", { writable: true });
-
-async function main() {
-  await core.ready();
-  // Show connection key on the frontend
-  document.getElementById("key").innerHTML =
-    "Your Hyperclip key is: " + b4a.toString(core.key, "hex");
-  swarm.join(core.discoveryKey);
-  swarm.on("connection", (conn) => core.replicate(conn));
-}
-
 // This function will push new data to the core
 function append(data) {
   core.append(data);
 }
+
+const swarm = new Hyperswarm();
+// Initialise hypercore in hyperclip-dir
+const core = new Hypercore("./hyperclip-dir", { writable: true });
+
+await core.ready();
+// Show connection key on the frontend
+document.getElementById("key").innerHTML =
+  "Your Hyperclip key is: " + b4a.toString(core.key, "hex");
+swarm.join(core.discoveryKey);
+swarm.on("connection", (conn) => core.replicate(conn));
 
 // Push clipboard data when button is clicked
 document
@@ -37,5 +35,3 @@ document
         window.alert("Failed to send clipboard: " + err);
       });
   });
-
-main(); // Call the async function
